d2929a1a1c146f0864fa5912a9f8017e36bd2ae5
Earth <pimanus1234@gmail.com>
2021-10-29 16:43:53 +0700

    init coinsTable

--- kyuka/src/components/CoinsTable.js
+++ kyuka/src/components/CoinsTable.js
@@ -1,4 +1,4 @@
-import { createTheme } from '@material-ui/core';
+import { Container, createTheme, LinearProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
@@ -7,7 +7,9 @@ import { CryptoState } from '../CryptoContext';
 
 const CoinsTable = () => {
     const [coins, setCoins] = useState([]);
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState("");
+    
 
     const { currency } = CryptoState();
     
@@ -37,7 +39,48 @@ const CoinsTable = () => {
     return (
         <ThemeProvider theme={darkTheme}> 
             <Container style={{textAlign: "center"}}>
+                <Typography
+                    variant="h4"
+                    style={{ margin: 18, fintFamily: "Montserrat"}}>
+                    Cryptocurrency Prices by Market Cap
+                </Typography>
 
+                <TextField label="Search for your favorite Crypto Currency.."
+                    variant="outlined"
+                    style={{ marginBottom: 20, width: "100%" }}
+                    onChange = {(e) => setSearch(e.target.value)}
+                />
+
+                <TableContainer>
+                    {
+                        loading ? (
+                            <LinearProgress style = {{ backgroundColor: "gold"}}  />
+                        ) : (
+                                <Table>
+                                    <TableHead style={{ backgroundColor: "#EEBC1D"}}>
+                                        <TableRow>
+                                            {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                                                <TableCell
+                                                    style={{
+                                                        color: "black",
+                                                        fontWeight: "700",
+                                                        fontFamily: "Montserrat",
+                                                }}
+                                                    key={head}
+                                                    align={head === "Coin" ? "":"right"}
+                                                >
+                                                    {head}
+                                                    </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        
+                                    </TableBody>
+                                </Table>
+                        )
+                    }
+                    </TableContainer>
 
             </Container>
         </ThemeProvider>
