a68d9fb29242aa461404362809293413d0adf795
Earth <pimanus1234@gmail.com>
2021-10-31 23:25:16 +0700

    add graph data

--- kyuka/src/components/CoinInfo.js
+++ kyuka/src/components/CoinInfo.js
@@ -1,8 +1,10 @@
-import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core';
+import { CircularProgress, createTheme, makeStyles, ThemeProvider } from '@material-ui/core';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Classnames } from 'react-alice-carousel';
+import { Line } from 'react-chartjs-2';
 import { HistoricalChart } from '../config/api';
+import { chartDays } from '../config/data';
 import { CryptoState } from '../CryptoContext';
 
 const CoinInfo = ({ coin }) => {
@@ -31,7 +33,23 @@ const CoinInfo = ({ coin }) => {
         },
     });
 
-    const useStyles = makeStyles(() => ({
+    const useStyles = makeStyles((theme) => ({
+
+        container: {
+            width: "75%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: 25,
+            padding: 40,
+            [theme.breakpoints.down("md")]: {
+                width: "100%",
+                marginTop: 0,
+                padding: 20,
+                paddingTop: 0,
+            },
+        }
 
     }))
 
@@ -39,7 +57,65 @@ const CoinInfo = ({ coin }) => {
 
     return (
         <ThemeProvider theme={darkTheme}>
-            <div className={classes.container}></div>
+            <div className={classes.container}>
+
+                {
+                    !historicData ? (
+                        <CircularProgress
+                            style={{ color: "pink" }}
+                            size={250}
+                            thickness={1}
+                        />
+                    ) : (
+                            <>
+                                <Line
+                                
+                                    data={{
+                                        labels: historicData.map((coin) => {
+                                            let date = new Date(coin[0]);
+                                            let time =
+                                                date.getHours() > 12
+                                                    ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                                                    : `${date.getHours()}:${date.getMinutes()} AM`;
+                                            return days === 1 ? time : date.toLocaleDateString()
+                                            
+                                        }),
+                                        
+                                        datasets: [
+                                            {
+                                                data: historicData.map((coin) => coin[1]),
+                                                label: `Price ( Past ${days} Days) in ${currency}`,
+                                                borderColor: "#EEBC1D",
+                                            },
+                                    
+                                        ],
+
+                                    }}
+                                    options={{
+                                        elements: {
+                                            point: {
+                                                radius: 1,
+                                            },
+                                        },
+                                    }}
+                                
+                                />
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        marginTop: 20,
+                                        justifyContent: "space-around",
+                                        width:"100%",
+                                    }}
+                                >
+                                    {chartDays.map((day) => (
+                                        <button>{day.label}</button>
+                                    ))}
+                                </div>
+                            </>)
+                }
+
+            </div>
         </ThemeProvider>
     )
 }
